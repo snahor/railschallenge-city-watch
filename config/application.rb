1c3b2580264543ba54213d8e08d119acbc6616b2
Hans Roman <hans.r.69@gmail.com>
2015-04-20 18:53:45 -0500

    Modify scaffolding options.
    No specs, no minitest, no templates, no assets, no helpers, no fixtures

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,14 @@ Bundler.require(*Rails.groups)
 module CityWatch
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.assets false
+      g.template_engine nil
+      g.test_framework nil
+      g.fixture nil
+      g.spec nil
+      g.helper false
+    end
   end
 end
