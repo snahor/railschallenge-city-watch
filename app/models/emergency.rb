4fbf14c8e2fe92c869b760f9fc1fa604c6d97933
Hans Roman <hans.r.69@gmail.com>
2015-04-20 19:13:02 -0500

    Add Emergency model and schema
    Includes validations

---
+++ app/models/emergency.rb
@@ -0,0 +1,6 @@
+class Emergency < ActiveRecord::Base
+  validates :code, presence: true, uniqueness: true
+  validates :fire_severity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :police_severity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :medical_severity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
