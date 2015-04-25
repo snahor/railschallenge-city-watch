5240916417018f5e88ece44bf85594af2726f227
Hans Roman <hans.r.69@gmail.com>
2015-04-25 11:30:09 -0500

    Remove generated code

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -1,24 +1,13 @@
 class EmergenciesController < ApplicationController
   before_action :set_emergency, only: [:show, :update, :destroy]
 
-  # GET /emergencies
-  # GET /emergencies.json
   def index
     @emergencies = Emergency.all
   end
 
-  # GET /emergencies/1
-  # GET /emergencies/1.json
   def show
   end
 
-  # GET /emergencies/new
-  def new
-    @emergency = Emergency.new
-  end
-
-  # POST /emergencies
-  # POST /emergencies.json
   def create
     @emergency = Emergency.new(emergency_params)
     if @emergency.save(emergency_params)
@@ -28,8 +17,6 @@ class EmergenciesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /emergencies/1
-  # PATCH/PUT /emergencies/1.json
   def update
     if @emergency.update(emergency_params)
       render :show, status: :ok, location: @emergency
@@ -38,24 +25,17 @@ class EmergenciesController < ApplicationController
     end
   end
 
-  # DELETE /emergencies/1
-  # DELETE /emergencies/1.json
   def destroy
     @emergency.destroy
-    respond_to do |format|
-      format.html { redirect_to emergencies_url, notice: 'Emergency was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    head :no_content
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_emergency
     @emergency = Emergency.find_by!(code: params[:code])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def emergency_params
     allowed_parameters = [:fire_severity, :police_severity, :medical_severity, :full_response]
     if !@emergency || @emergency.new_record?
--- app/controllers/responders_controller.rb
+++ app/controllers/responders_controller.rb
@@ -1,8 +1,6 @@
 class RespondersController < ApplicationController
   before_action :set_responder, only: [:show, :edit, :update, :destroy]
 
-  # GET /responders
-  # GET /responders.json
   def index
     if params[:show] == 'capacity'
       render json: { capacity: Responder.capacity_summary }
@@ -11,22 +9,9 @@ class RespondersController < ApplicationController
     end
   end
 
-  # GET /responders/1
-  # GET /responders/1.json
   def show
   end
 
-  # GET /responders/new
-  def new
-    @responder = Responder.new
-  end
-
-  # GET /responders/1/edit
-  def edit
-  end
-
-  # POST /responders
-  # POST /responders.json
   def create
     @responder = Responder.new(responder_params)
 
@@ -37,38 +22,25 @@ class RespondersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /responders/1
-  # PATCH/PUT /responders/1.json
   def update
-    respond_to do |format|
-      if @responder.update(responder_params)
-        format.html { redirect_to @responder, notice: 'Responder was successfully updated.' }
-        format.json { render :show, status: :ok, location: @responder }
-      else
-        format.html { render :edit }
-        format.json { render json: @responder.errors, status: :unprocessable_entity }
-      end
+    if @responder.update(responder_params)
+      render :show, status: :ok, location: @responder
+    else
+      render json: @responder.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /responders/1
-  # DELETE /responders/1.json
   def destroy
     @responder.destroy
-    respond_to do |format|
-      format.html { redirect_to responders_url, notice: 'Responder was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    head :no_content
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_responder
     @responder = Responder.eager_load(:emergency).find_by!(name: params[:name])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def responder_params
     if !@responder || @responder.new_record?
       allowed_parameters = [:name, :capacity, :type]
